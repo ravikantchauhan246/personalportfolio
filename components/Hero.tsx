f2e11923e795d059fe9a9447b27f6d1100d743df
ravikantchauhan246 <ravikantchauhan246@gmail.com>
2024-09-19 12:06:52 +0530

    add hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   const words = `Crafting intuitive web solutions that bring ideas to life.
@@ -25,9 +27,16 @@ const Hero = () => {
         <div className="flex justify-center relative my-20 z-10">
           <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
             <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words={words} />
-            <p className="text-center">
-                Hi, I&apos;m Ravikant, a Web Developer based in India
+            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+                Hi, I&apos;m Ravikant, a Web Developer based in India.
             </p>
+            <a href="#about">
+              <MagicButton
+              title="Show my work" 
+              icon={<FaLocationArrow/>}
+              position="right"
+              />
+            </a>
           </div>
         </div>
       </div>

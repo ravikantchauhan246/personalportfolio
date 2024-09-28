c1e330c1752aba7dabc2aeb07edd626d165ff4fc
ravikantchauhan246 <ravikantchauhan246@gmail.com>
2024-09-28 21:25:09 +0530

    add navbar and bento grid

---
+++ data/index.ts
@@ -0,0 +1,7 @@
+export const gridItems = [
+  { title: "Title1", description: "Desc1", id: 1 },
+  { title: "Title2", description: "Desc2", id: 2 },
+  { title: "Title3", description: "Desc3", id: 3 },
+  { title: "Title4", description: "Desc4", id: 4 },
+  { title: "Title5", description: "Desc5", id: 5 },
+];

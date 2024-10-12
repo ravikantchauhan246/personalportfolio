fe55373a897253a01b42197ef5ed724c4a63fa05
ravikantchauhan246 <ravikantchauhan246@gmail.com>
2024-10-12 07:45:17 +0530

    bento grid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export const BentoGrid = ({
   className,
@@ -23,28 +24,43 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
-  icon,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   id,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
+ 
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
-  header?: React.ReactNode;
-  icon?: React.ReactNode;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
   id:number;
 }) => {
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 border-red-500",
+        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
+      style={{
+        background: 'rgb(4,7,29)',
+        backgroundColor: 'linear-gradient(90deg, rgba(8,6,57,1) 9%, rgba(6,77,166,1) 59%, rgba(0,212,255,1) 100%)',
+      }}  
     >
-      {header}
+      <div className={`${id === 6} && 'flex justify-center h-full'`}>
+        <div className="w-full h-full">
+         {img && (
+          <img src={img} alt={title} className={cn(imgClassName,'object-cover,object-center')}/>
+         )}
+        </div>
+      </div>
+      
       <div className="group-hover/bento:translate-x-2 transition duration-200">
-        {icon}
+       
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
         </div>
